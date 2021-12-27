a76caf8025f28fb4886683fcf604c9db2ec16078
juanbep <jbecapillimue@gmail.com>
2021-12-27 16:04:54 -0500

    v 1.3

---
+++ src/Entity/Producto.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductoRepository::class)
+ */
+class Producto
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=250)
+     */
+    private $nombre;
+    
+    /**
+     * @ORM\Column(type="string", length=250)
+     */
+    private $codigo;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+    
+    function getCodigo(): ?string
+    {
+        return $this->codigo;
+    }
+
+    function setCodigo($codigo): self
+    {
+        $this->codigo = $codigo;
+        
+        return $this;
+    }
+
+
+    
+}
